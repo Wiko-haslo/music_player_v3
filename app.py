5ea387a62d82e28302c4c3c64350a6a228c2ea68
wiko_haslo <wiko.antczak@gmail.com>
2025-04-08 16:31:59 +0200

    Remove app.run() in production to use WSGI server

--- app.py
+++ app.py
@@ -376,6 +376,5 @@ if __name__ == '__main__':
     app.run(debug=True)
 else:
     # Dla Render
-    import os
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    if __name__ == '__main__':
+    app.run(debug=True)
